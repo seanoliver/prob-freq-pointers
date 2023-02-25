fa0315bc0b12cc4d1d0b8a8a5c4b5d2ee48dfc6a
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-02-24 17:26:57 -0800

    isSubsequence solution

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+function isSubsequence(s1, s2) {
+
+  let s1Index = 0;
+  let s2Index = 0;
+
+  while (s1Index < s1.length && s2Index < s2.length) {
+
+    if (s1[s1Index] === s2[s2Index]) {
+      s1Index++;
+    }
+    s2Index++;
+
+  }
+  return s1.length === s1Index;
+
 }
